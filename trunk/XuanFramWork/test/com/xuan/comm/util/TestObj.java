276625be6f7d67cb236c97bf20b347807741e3dc
XieZhaoXuan@gmail.com <XieZhaoXuan@gmail.com@e1354506-7d07-c174-5d5b-2f9a4df38202>
2012-03-11 09:29:37 +0000

    更新代码，同步

--- trunk/XuanFramWork/test/com/xuan/comm/util/TestObj.java
+++ trunk/XuanFramWork/test/com/xuan/comm/util/TestObj.java
@@ -1,9 +1,24 @@
 package com.xuan.comm.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
+
 public class TestObj {
 	private long ids;
 	private String name;
 	private int age;
+	private List<TestObj> children = new ArrayList<TestObj>();
+
+	public List<TestObj> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<TestObj> children) {
+		this.children = children;
+	}
 
 	public TestObj() {
 		super();
@@ -38,5 +53,32 @@ public class TestObj {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+//	public String getGridJSON() {
+//		JsonConfig jsonConfig = new JsonConfig();
+//		JSONObject json = JSONObject.fromObject(this, jsonConfig);
+//		return json.toString();
+//	}
+
+	public static void main(String[] args) {
+		TestObj f = new TestObj();
+		f.setIds(1);
+		f.setName("父节点");
+		
+		TestObj c1 = new TestObj();
+		c1.setIds(10);
+		c1.setName("子节点1");
+		
+		TestObj c2 = new TestObj();
+		c2.setIds(11);
+		c2.setName("子节点2");
+		
+		f.getChildren().add(c1);
+		f.getChildren().add(c2);
+		
+		JsonConfig jsonConfig = new JsonConfig();
+		JSONObject json = JSONObject.fromObject(f, jsonConfig);
+		System.out.print(json.toString());
 	}
 }
